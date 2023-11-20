a7da4764e493269aa7cd4282d42532ebb8837625
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-11-20 17:40:30 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `jffrsnpbbr.github.io`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
